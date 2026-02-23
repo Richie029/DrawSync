9ca15e5238f529fe12a374a5bfb14f0d7e3f345e
Richie029 <codeblings@gmail.com>
2026-02-23 19:25:53 +0530

    Completed Signup Endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,16 +3,31 @@ import jwt from 'jsonwebtoken';
 import {JWT_SECRET} from '@repo/backend-common/config';
 import { middleware } from './middleware';
 import {CreateUserSchema,SigninSchema,CreateRoomSchema} from '@repo/common/types';
+import {prismaClient} from "@repo/db/client";
+
 const app = express();
 
-app.post('/signup', (req, res) => {
-    const data = CreateUserSchema.safeParse(req.body);
-    if (!data.success) {
+app.post('/signup', async(req, res) => {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
         return res.status(400).json({
            message: "Incorrect inputs",
         })
     }
     //db call
+    try{
+        await prismaClient.user.create({
+            data:{
+                email:parsedData.data?.username,
+                password:parsedData.data.password,
+                name:parsedData.data.name
+            }
+        });
+    }catch(e){
+        return res.status(500).json({
+            message: "User already exists with this email"
+        })
+    }
     res.json({
         "userId":123
     })
